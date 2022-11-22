b4b4a34b69129ee1449e05d884b7283ac7de317b
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro.local>
2022-11-22 13:17:12 -0800

    adding line parsing and debug mode

--- design-uber/uber-array-design.py
+++ design-uber/uber-array-design.py
@@ -26,6 +26,8 @@ def main():
 	# Outputs
 	parser.add_argument("--out", help="Prefix for output files", \
 		type=str, required=True)
+	# Extra optional arguments
+	parser.add_argument("--debug", help="Print helpful debug info", action="store_true")
 	## Parse the arguments
 	args = parser.parse_args()
 	if not os.path.exists(args.reffa):
@@ -39,6 +41,20 @@ def main():
 	sys.stderr.write("Using reference genome: " + args.reffa + "\n")
 	sys.stderr.write("Using repeats bed: " + args.rptsbed + "\n")
 	sys.stderr.write("Using output prefix: " + args.out + "\n")
+
+	# Process one STR locus at a time
+	with open(args.rptsbed, "r") as f:
+		for line in f:
+			# Extract relevant info for the STR
+			items = line.strip().split()
+			chrom = items[0]
+			str_start = int(items[1])
+			str_end = int(items[2])
+			repeat_unit = items[3]
+
+			if args.debug:
+				sys.stderr.write("Processing %s:%s-%s repeat unit=%s\n"%(chrom, str_start, \
+						str_end, repeat_unit))
 
 	sys.exit(0)
 
